5a5b1365b715045223faa168a1c0b222887f4344
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-10-04 10:36:30 -0700

    Added Doggo migration

---
+++ AnimalRescueWebsite/Data/Migrations/20231004173236_Doggo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimalRescueWebsite.Data.Migrations
+{
+    public partial class Doggo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dogs",
+                columns: table => new
+                {
+                    DogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DogName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DogAge = table.Column<byte>(type: "tinyint", nullable: false),
+                    DogSize = table.Column<double>(type: "float", nullable: false),
+                    DogBreed = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DogGender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DogPic = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dogs", x => x.DogId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dogs");
+        }
+    }
+}
