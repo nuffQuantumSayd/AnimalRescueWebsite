1dd1de9f7f83e943207c41dac0c0866c82604403
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-10-31 14:15:13 -0700

    Added data annotations for the display names of the applicant class

--- AnimalRescueWebsite/Models/Applicant.cs
+++ AnimalRescueWebsite/Models/Applicant.cs
@@ -12,64 +12,77 @@ namespace AnimalRescueWebsite.Models
         /// The unique identifier for each application
         /// </summary>
         [Key]
+        [Display(Name = "Applicantion ID")]
         public int ApplicantId { get; set; }
 
         /// <summary>
         /// The first name of the applicant
         /// </summary>
+        [Display(Name = "First Name")]
         public string ApplicantFirstName { get; set; }
 
         /// <summary>
         /// The last name of the applicant
         /// </summary>
+        [Display(Name = "Last Name")]
         public string ApplicantLastName { get; set; }
 
         /// <summary>
         /// The date of the application
         /// </summary>
+        [Display(Name = "Date of Application")]
         public DateTime DateOfApplication { get; set; }
 
         /// <summary>
         /// The street of the address the applicant resides in
         /// </summary>
+        [Display(Name = "Street")]
         public string AddressStreet { get; set; }
 
         /// <summary>
         /// The city of the address the applicant resides in
         /// </summary>
+        [Display(Name = "City")]
         public string AddressCity { get; set; }
 
         /// <summary>
         /// The state of the address the applicant resides in
         /// </summary>
+        [Display(Name = "State")]
         public string AddressState { get; set; }
 
         /// <summary>
         /// The zip code of the address the applicant resides in
         /// </summary>        
+        [Display(Name = "Zip Code")]
         public string AddressZip { get; set; }
 
         /// <summary>
         /// The email of the applicant
         /// </summary>
+        [Display(Name = "Email")]
         public string ApplicantEmail { get; set; }
 
         /// <summary>
         /// The primary phone number of the applicant 
         /// </summary>
+        [Display(Name = "Phone Number")]
         public string ApplicantPhone { get; set; }
 
         /// <summary>
         /// The occupation of the applicant
         /// </summary>
+        [Display(Name = "Occupation")]
         public string ApplicantOccupation { get; set; }
 
         /// <summary>
         /// The number of children an applicant has
         /// </summary>
+        [Display(Name = "Number of Children")]
         public int NumberOfChildren { get; set; }
 
         [ForeignKey(nameof(Dog))]
+        [Display(Name = "Dog Id")]
         public int dog { get; set; }
     }
 }
