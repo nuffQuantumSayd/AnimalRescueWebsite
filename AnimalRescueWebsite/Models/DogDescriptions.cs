31039add15304a6551e13ff2b4163f3a9e9dc917
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-11-12 14:30:06 -0800

    Created a dog descriptions class with a foreign key linking to dog class

---
+++ AnimalRescueWebsite/Models/DogDescriptions.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AnimalRescueWebsite.Models
+{
+    public class DogDescriptions
+    {
+        [ForeignKey(nameof(Dog))]
+        public int DescriptionId { get; set; }
+
+        public string Characteristics { get; set; }
+
+        public string CoatLength { get; set; }
+
+        public string Health { get; set; }
+
+        public string GoodInHomeWith { get; set; }
+
+        public double AdoptionFee { get; set; }
+
+        public string LongDescription { get; set; }
+    }
+}
