233e75d61f9f41e309b47fbe04d20ed142d785e6
Aaron Kornish <aaronkornish.ak@gmail.com>
2023-11-12 17:38:22 -0800

    Update DogTablePopulation.sql
    Added two more dogs to the population

--- DogTablePopulation.sql
+++ DogTablePopulation.sql
@@ -4,4 +4,6 @@ values('Pee Wee', 11, 5.9, 'Chihuahua', 'Male', 'Likes to bite'),
 	('Megan', 12, 4.6, 'Chihuahua', 'Female', 'Very shy'),
 	('Pippin', 12, 15, 'Poodle', 'Male', 'Hypoalergenic'),
 	('Gatsby', 12, 15, 'Poodle', 'Male', 'Hypoalergenic'),
-	('Joey', 10, 15, 'Pug', 'Male', 'Bug-eyed')
+	('Joey', 10, 15, 'Pug', 'Male', 'Bug-eyed'),
+	('Willie', 16, 15, 'Rat Terrier', 'Male', 'Very sweet'),
+	('Auggie', 16, 15, 'Rat Terreir', 'Male', 'Sour then sweet')
